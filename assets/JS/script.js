d492658352df322d4cb8177bbe812540421ab87a
andreaconv <andrea2convertini@gmail.com>
2023-04-03 16:34:37 +0200

    end milestone 1

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,10 +1,48 @@
 
-const team = {
-  wayne: {
+const team = [
+  {
     firstname: "Wayne Barnett",
     role: "Founder & CEO",
     image: "assets/img/wayne-barnett-founder-ceo.jpg"
+  },
+
+  {
+    firstname: "Angela Caroll",
+    role: "Chief Editor",
+    image: "assets/img/angela-caroll-chief-editor.jpg"
+  },
+
+  {
+    firstname: "Walter Gordon",
+    role: "Office Manager",
+    image: "assets/img/walter-gordon-office-manager.jpg"
+  },
+
+  {
+    firstname: "Angela Lopez",
+    role: "Social Media Manager",
+    image: "assets/img/angela-lopez-social-media-manager.jpg"
+  },
+
+  {
+    firstname: "Scott Estrada",
+    role: "Developer",
+    image: "assets/img/scott-estrada-developer.jpg"
+  },
+
+  {
+    firstname: "Barbara Ramos",
+    role: "Graphic Designer",
+    image: "assets/img/barbara-ramos-graphic-designer.jpg"
   }
-}
+]
 console.log(team)
-console.log(team.wayne)
+console.warn("primo membro", team[0])
+
+
+//ciclo per stampare tutti i membri "for in"
+for(let i in team){
+  console.log("indice", i, "membro", team[i])
+}
+
+//---------------------------------------------------
