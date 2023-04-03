14040347377384c31106993de80316707f932986
andreaconv <andrea2convertini@gmail.com>
2023-04-03 16:03:01 +0200

    verifica milestone 1

---
+++ assets/JS/script.js
@@ -0,0 +1,10 @@
+
+const team = {
+  wayne: {
+    firstname: "Wayne Barnett",
+    role: "Founder & CEO",
+    image: "assets/img/wayne-barnett-founder-ceo.jpg"
+  }
+}
+console.log(team)
+console.log(team.wayne)
